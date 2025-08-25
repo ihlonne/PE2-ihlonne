019f34957f9802b48cfd0119b31c305c5e35274c
Ingelinn Helene <ingelinn92@gmail.com>
2025-08-25 15:33:10 +0200

    add provider component

--- pe2-ihlonne/src/main.tsx
+++ pe2-ihlonne/src/main.tsx
@@ -1,15 +1,19 @@
 import { StrictMode } from 'react';
+
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
 import '@fontsource-variable/albert-sans/index.css';
 import '@fontsource/inria-serif/index.css';
+import { Provider } from './components/ui/provider.tsx';
 
 createRoot(
   document.getElementById('root')!
 ).render(
   <StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </StrictMode>
 );
