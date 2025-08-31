cba9a36fbf9b09856869eba9feb90c7d524a0778
Ingelinn Helene <ingelinn92@gmail.com>
2025-08-31 11:39:35 +0200

    move hero section to own component

---
+++ src/components/homePage/HeroSection.tsx
@@ -0,0 +1,50 @@
+import {
+  Box,
+  Container,
+  Heading,
+} from '@chakra-ui/react';
+
+import Search from '../Search';
+import hero from '../../assets/heroImage.jpg';
+
+const HeroSection = () => {
+  return (
+    <Box
+      display='flex'
+      alignItems='center'
+      justifyContent='center'
+      position='relative'
+      backgroundImage={`url(${hero})`}
+      backgroundSize='cover'
+      backgroundPosition='center'
+      backgroundRepeat='no-repeat'
+      h='100vh'
+      w='100%'
+      _before={{
+        content: '""',
+        position: 'absolute',
+        inset: 0,
+        bg: 'blackAlpha.600',
+      }}
+    >
+      <Container
+        position='relative'
+        zIndex={1}
+        textAlign='center'
+        color='white'
+      >
+        <Heading
+          as='h1'
+          fontSize='5xl'
+          mb={6}
+          fontStyle='italic'
+        >
+          Where to next?
+        </Heading>
+        <Search />
+      </Container>
+    </Box>
+  );
+};
+
+export default HeroSection;
