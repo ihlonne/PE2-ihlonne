312a11e9f397856c1c518377bdd41de9fe53a335
Ingelinn Helene <ingelinn92@gmail.com>
2025-09-13 17:15:05 +0200

    change box to flex

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,67 +21,81 @@ const Header: React.FC = () => {
     useState(false);
 
   return (
-    <Box
+    <Flex
       as='header'
       position='fixed'
-      display='flex'
-      justifyContent='space-between'
+      justifyContent='center'
       w='100%'
-      h='auto'
-      p='2rem 6rem'
+      h='100px'
+      top='0'
+      py='2rem'
+      px={{ base: '4', xl: 0 }}
       zIndex='1000'
     >
-      <Box>
-        <Image src={logo} />
-      </Box>
       <Flex
-        as='ul'
-        listStyleType='none'
-        alignItems='center'
-        gap='10'
+        justifyContent='space-between'
+        w='100%'
+        maxW='1290px'
       >
-        <Box as='li' fontWeight='semibold'>
-          <Link to='/Venues'> Browse Venues</Link>
+        <Box>
+          <Image src={logo} />
         </Box>
-        <Box as='li' fontWeight='semibold'>
-          <Text
-            onClick={() => setRegisterOpen(true)}
+        <Flex
+          as='ul'
+          listStyleType='none'
+          alignItems='center'
+          gap='10'
+        >
+          <Box as='li' fontWeight='semibold'>
+            <Link to='/Venues'>
+              {' '}
+              Browse Venues
+            </Link>
+          </Box>
+          <Box as='li' fontWeight='semibold'>
+            <Text
+              onClick={() =>
+                setRegisterOpen(true)
+              }
+            >
+              List Your Venue
+            </Text>
+          </Box>
+          <Box
+            bg='brand700'
+            color='white'
+            p='0.25rem 1.25rem'
+            rounded='md'
+            cursor='pointer'
+            onClick={() => setLoginOpen(true)}
           >
-            List Your Venue
-          </Text>
-        </Box>
-        <Box
-          bg='brand700'
-          color='white'
-          p='0.25rem 1.25rem'
-          rounded='md'
-          cursor='pointer'
-          onClick={() => setLoginOpen(true)}
-        >
-          Log In
-        </Box>
-        <CustomModal
-          open={registerOpen}
-          onClose={() => setRegisterOpen(false)}
-          title='Create account'
-        >
-          <RegisterForm
-            onSuccess={() =>
-              setRegisterOpen(false)
-            }
-          />
-        </CustomModal>
-        <CustomModal
-          open={loginOpen}
-          onClose={() => setLoginOpen(false)}
-          title='Login to your account'
-        >
-          <LoginForm
-            onSuccess={() => setLoginOpen(false)}
-          />
-        </CustomModal>
+            Log In
+          </Box>
+          <CustomModal
+            open={registerOpen}
+            onClose={() => setRegisterOpen(false)}
+            title='Create account'
+          >
+            <RegisterForm
+              onSuccess={() =>
+                setRegisterOpen(false)
+              }
+            />
+          </CustomModal>
+          <CustomModal
+            open={loginOpen}
+            onClose={() => setLoginOpen(false)}
+            title='Login to your account'
+          >
+            <LoginForm
+              onSuccess={() =>
+                setLoginOpen(false)
+              }
+            />
+          </CustomModal>
+        </Flex>
       </Flex>
-    </Box>
+    </Flex>
   );
 };
 
