45a8a9706ccef49ac1790a136ae43ce4e6b7774b
Ingelinn Helene <ingelinn92@gmail.com>
2025-09-13 17:09:59 +0200

    import toaster + venue page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { Toaster } from './components/ui/toaster';
+
 import {
   BrowserRouter,
   Route,
   Routes,
 } from 'react-router';
+
 import Layout from './components/Layout';
 import Home from './pages/Home';
-import { Toaster } from './components/ui/toaster';
+import Venue from './pages/venues/Venue';
 
 const App = function () {
   return (
@@ -14,6 +17,10 @@ const App = function () {
         <Layout>
           <Routes>
             <Route path='/' element={<Home />} />
+            <Route
+              path='/venues/venue/:id'
+              element={<Venue />}
+            />
           </Routes>
         </Layout>
       </BrowserRouter>
