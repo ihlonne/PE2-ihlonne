40a8a78451b6a25312e4a249789711d4d8d00b88
Ingelinn Helene <ingelinn92@gmail.com>
2025-09-14 14:37:15 +0200

    add login/logout logic

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
 import type React from 'react';
 import {
+  Avatar,
+  AvatarGroup,
   Box,
   Flex,
   Image,
@@ -8,11 +10,17 @@ import {
 
 /* import logo from '../assets/lightlogo.png'; */
 import logo from '../assets/darklogo.png';
-import { Link } from 'react-router';
-import { useState } from 'react';
+import { Link, useNavigate } from 'react-router';
+import {
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import CustomModal from './CustomModal';
 import { RegisterForm } from '../features/auth/RegisterForm';
 import { LoginForm } from '../features/auth/LoginForm';
+import { useAuth } from '../hooks/useAuth';
+import Navbar from './layout/Navbar';
 
 const Header: React.FC = () => {
   const [registerOpen, setRegisterOpen] =
@@ -20,14 +28,58 @@ const Header: React.FC = () => {
   const [loginOpen, setLoginOpen] =
     useState(false);
 
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const [isProfileNavOpen, setIsProfileNavOpen] =
+    useState(false);
+
+  const menuRef = useRef<HTMLDivElement | null>(
+    null
+  );
+  const avatarRef = useRef<HTMLDivElement | null>(
+    null
+  );
+
+  useEffect(() => {
+    const handleClickOutside = (
+      e: MouseEvent
+    ) => {
+      const target = e.target as Node;
+      if (
+        isProfileNavOpen &&
+        menuRef.current &&
+        !menuRef.current.contains(target) &&
+        avatarRef.current &&
+        !avatarRef.current.contains(target)
+      ) {
+        setIsProfileNavOpen(false);
+      }
+    };
+
+    document.addEventListener(
+      'mousedown',
+      handleClickOutside
+    );
+    return () =>
+      document.removeEventListener(
+        'mousedown',
+        handleClickOutside
+      );
+  }, [isProfileNavOpen]);
+
+  const handleLogout = () => {
+    logout();
+    setIsProfileNavOpen(false);
+    navigate('/');
+  };
+
   return (
     <Flex
       as='header'
-      position='fixed'
       justifyContent='center'
       w='100%'
       h='100px'
-      top='0'
       py='2rem'
       px={{ base: '4', xl: 0 }}
       zIndex='1000'
@@ -37,9 +89,9 @@ const Header: React.FC = () => {
         w='100%'
         maxW='1290px'
       >
-        <Box>
+        <Link to='/'>
           <Image src={logo} />
-        </Box>
+        </Link>
         <Flex
           as='ul'
           listStyleType='none'
@@ -52,25 +104,60 @@ const Header: React.FC = () => {
               Browse Venues
             </Link>
           </Box>
-          <Box as='li' fontWeight='semibold'>
-            <Text
-              onClick={() =>
-                setRegisterOpen(true)
-              }
-            >
-              List Your Venue
-            </Text>
-          </Box>
-          <Box
-            bg='brand700'
-            color='white'
-            p='0.25rem 1.25rem'
-            rounded='md'
-            cursor='pointer'
-            onClick={() => setLoginOpen(true)}
-          >
-            Log In
-          </Box>
+          {!user ? (
+            <Box>
+              <Box as='li' fontWeight='semibold'>
+                <Text
+                  onClick={() =>
+                    setRegisterOpen(true)
+                  }
+                >
+                  List Your Venue
+                </Text>
+              </Box>
+              <Box
+                bg='brand700'
+                color='white'
+                p='0.25rem 1.25rem'
+                rounded='md'
+                cursor='pointer'
+                onClick={() => setLoginOpen(true)}
+              >
+                Log In
+              </Box>
+            </Box>
+          ) : (
+            <Box>
+              <AvatarGroup
+                ref={avatarRef}
+                onClick={() =>
+                  setIsProfileNavOpen(
+                    (prev) => !prev
+                  )
+                }
+                cursor='pointer'
+                aria-label='Profile menu'
+              >
+                <Avatar.Root>
+                  <Avatar.Fallback
+                    name={user.name}
+                  />
+                  <Avatar.Image
+                    src={user.avatar?.url}
+                    alt={user.avatar?.alt}
+                  />
+                </Avatar.Root>
+              </AvatarGroup>
+              {isProfileNavOpen && (
+                <Box ref={menuRef}>
+                  <Navbar
+                    onLogout={handleLogout}
+                  />
+                </Box>
+              )}
+            </Box>
+          )}
+
           <CustomModal
             open={registerOpen}
             onClose={() => setRegisterOpen(false)}
