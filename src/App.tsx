abae7c2dcf90a05b82fe5895f6e89e26654f1264
Ingelinn Helene <ingelinn92@gmail.com>
2025-09-18 08:47:44 +0200

    import Venues

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import {
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Venue from './pages/venues/Venue';
+import Venues from './pages/venues/Venues';
 
 const App = function () {
   return (
@@ -20,6 +21,10 @@ const App = function () {
             <Route
               path='/venues/venue/:id'
               element={<Venue />}
+            />
+            <Route
+              path='/venues'
+              element={<Venues />}
             />
           </Routes>
         </Layout>
