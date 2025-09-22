d0536a76b6e293739c9ac0e16931b9c5d65e253a
Ingelinn Helene <ingelinn92@gmail.com>
2025-09-22 11:58:31 +0200

    create booking card

--- src/components/CustomerBookingCard.tsx
+++ src/components/CustomerBookingCard.tsx
@@ -3,12 +3,14 @@ import {
   Button,
   Flex,
   Heading,
+  Image,
   Span,
   Text,
 } from '@chakra-ui/react';
 import { IoLocationSharp } from 'react-icons/io5';
 import type { Booking } from '../types/booking';
 import { useNavigate } from 'react-router';
+import { formatDate } from '../utils/dates';
 
 interface CustomerBookingCardProps {
   booking: Booking;
@@ -20,67 +22,110 @@ const CustomerBookingCard = ({
   onCancel,
 }: CustomerBookingCardProps) => {
   const navigate = useNavigate();
+  console.log(booking);
   return (
     <Flex
-      direction='column'
+      direction={{ base: 'column', lg: 'row' }}
       gap='2rem'
       bg='brand200'
       p='1.5rem'
       rounded='lg'
       w='100%'
     >
-      <Box>
-        <Heading as='h2' fontSize='md'>
-          {booking.venue!.name}
-        </Heading>
-        <Flex alignItems='center' gap='2'>
-          <IoLocationSharp />
-          <Text fontSize='xs'>
-            {booking.venue?.location?.address}
+      <Box
+        w={{ base: '100%', md: '150px' }}
+        h='150px'
+      >
+        <Image
+          src={
+            booking.venue?.media?.[0]?.url ??
+            'https://images.pexels.com/photos/28216688/pexels-photo-28216688.jpeg'
+          }
+          alt={
+            booking.venue?.media?.[0]?.alt ??
+            'Default venue image'
+          }
+          w='100%'
+          h='100%'
+        />
+      </Box>
+      <Flex
+        direction='column'
+        justifyContent='space-between'
+        w='full'
+        gap={{ base: '1rem', lg: 0 }}
+      >
+        <Box>
+          <Heading as='h2' fontSize='md'>
+            {booking.venue!.name}
+          </Heading>
+          <Flex alignItems='center' gap='2'>
+            <IoLocationSharp />
+            <Text fontSize='xs'>
+              {booking.venue?.location?.address}
+            </Text>
+          </Flex>
+        </Box>
+
+        <Flex
+          direction={{
+            base: 'column',
+            lg: 'row',
+          }}
+          gap={{ base: 0, md: '2rem' }}
+          w='full'
+        >
+          <Text>
+            <Span fontWeight='semibold'>
+              Guests:
+            </Span>{' '}
+            {booking.guests}
+          </Text>
+          <Text>
+            <Span fontWeight='semibold'>
+              From:
+            </Span>{' '}
+            {formatDate(booking.dateFrom)}
+          </Text>
+          <Text>
+            <Span fontWeight='semibold'>To:</Span>{' '}
+            {formatDate(booking.dateTo)}
+          </Text>
+          <Text>
+            <Span fontWeight='semibold'>
+              Booking reference:
+            </Span>{' '}
+            {booking.id.slice(0, 6)}
           </Text>
         </Flex>
-      </Box>
 
-      <Flex direction='column'>
-        <Text>
-          <Span fontWeight='semibold'>
-            Guests:
-          </Span>{' '}
-          {booking.guests}
-        </Text>
-        <Text>
-          <Span fontWeight='semibold'>From:</Span>{' '}
-          {booking.dateFrom}
-        </Text>
-        <Text>
-          <Span fontWeight='semibold'>To:</Span>{' '}
-          {booking.dateTo}
-        </Text>
-      </Flex>
-
-      <Text>
-        <Span fontWeight='semibold'>
-          Booking reference:
-        </Span>{' '}
-        {booking.id.slice(0, 6)}
-      </Text>
-
-      <Flex w='full' gap='2'>
-        <Button
-          bg='brand700'
-          color='white'
-          fontWeight='semibold'
-          onClick={() =>
-            navigate(
-              `/venues/venue/${booking.venue?.id}`
-            )
-          }
+        <Flex
+          direction={{
+            base: 'column',
+            md: 'row',
+          }}
+          w='full'
+          gap='2'
         >
-          View Venue
-        </Button>
-        <Button bg='brand300' onClick={onCancel}>
-          Cancel Booking
-        </Button>
+          <Button
+            bg='brand700'
+            color='white'
+            fontWeight='semibold'
+            onClick={() =>
+              navigate(
+                `/venues/venue/${booking.venue?.id}`
+              )
+            }
+          >
+            View Venue
+          </Button>
+          <Button
+            bg='brand300'
+            onClick={onCancel}
+          >
+            Cancel Booking
+          </Button>
+        </Flex>
       </Flex>
     </Flex>
   );
