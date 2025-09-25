972feb2ef2430a0be3ee90e971e5a312fedd87e2
Ingelinn Helene <ingelinn92@gmail.com>
2025-09-25 11:48:03 +0200

    fix position

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -12,6 +12,7 @@ import { LuLogOut } from 'react-icons/lu';
 import { IoIosBrowsers } from 'react-icons/io';
 import { Link, useNavigate } from 'react-router';
 import { useAuth } from '../../hooks/useAuth';
+import { useRef } from 'react';
 
 interface NavbarProps {
   onLogout: () => void;
@@ -26,17 +27,22 @@ const Navbar: React.FC<NavbarProps> = ({
   const handleNavigation = (path: string) => {
     navigate(path);
   };
-
+  const menuRef = useRef<HTMLDivElement | null>(
+    null
+  );
   return (
     <Box
+      ref={menuRef}
       position='absolute'
+      top='100%'
+      right={0}
+      mt='0.5rem'
       bg='white'
       p='4'
-      maxW='200px'
-      w='100%'
-      right='20rem'
+      w='200px'
       border='1px solid #eee'
       rounded='md'
+      boxShadow='0 8px 24px rgba(18, 13, 30, 0.15)'
       zIndex='3000'
     >
       <Stack>
